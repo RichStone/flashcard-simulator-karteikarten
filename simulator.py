37d48369f16b7e3ee26f9cc8ef6c30b2bc1e07d9
RichStone <richard.had@hotmail.de>
2020-01-15 23:35:49 +0100

    add learn and get cards functionality

--- simulator.py
+++ simulator.py
@@ -1,6 +1,11 @@
+from uuid import uuid1
+
+
 class Simulator:
-    def __init__(self):
-        pass
+    def __init__(self, card_amount):
+        # days is the 'calendar'
+        # it's a list of lists filled with cards to learn
+        self.days = [[Card()] for _ in range(card_amount)]
 
     def simulate(self):
         pass
@@ -13,7 +18,16 @@ class Simulator:
     def forgot(card):
         card.learn_progress = 0
 
+    def get_cards(self, day):
+        return self.days[day - 1]
+
+    def learn(self, day):
+        cards = self.get_cards(day)
+        for card in cards:
+            self.days[2].append(card)
+
 
 class Card:
     def __init__(self):
+        self.id = uuid1
         self.learn_progress = 0
