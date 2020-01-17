078cfef8a4d2949437f9474e5d1ea691254065cb
RichStone <richard.had@hotmail.de>
2020-01-17 21:18:11 +0100

    implement dynamic coding algorithm

--- simulator.py
+++ simulator.py
@@ -4,8 +4,14 @@ from uuid import uuid1
 class Simulator:
     def __init__(self, card_amount):
         # days is the 'calendar'
-        # it's a list of lists filled with cards to learn
+        # it's a list of lists filled with one new Card per day to learn
         self.days = [[Card()] for _ in range(card_amount)]
+        self.learn_rhythm = {
+            0: 1,
+            1: 3,
+            2: 7,
+            3: 30,
+        }
 
     def simulate(self):
         pass
@@ -21,21 +27,16 @@ class Simulator:
     def get_cards(self, day_idx):
         return self.days[day_idx]
 
-    def learn(self, day):
-        cards = self.get_cards(day)
+    def learn(self, current_day):
+        cards = self.get_cards(current_day)
         for card in cards:
-            if card.learn_progress == 0:
-                try:
-                    self.days[day + 3].append(card)
-                except IndexError:
-                    print('not enough calendar places initialized')
-            if card.learn_progress == 1:
-                try:
-                    if self.days[day + 7]:
-                        self.days[day + 7].append(card)
-                except IndexError:
-                    print('not enough calendar places initialized')
             Simulator.learned(card)
+            if card.learn_progress in self.learn_rhythm:
+                try:
+                    next_learning_day = current_day + self.learn_rhythm[card.learn_progress]
+                    self.days[next_learning_day].append(card)
+                except IndexError:
+                    print('not enough calendar places initialized')
 
     def print_cards(self):
         for i, day in enumerate(self.days):
